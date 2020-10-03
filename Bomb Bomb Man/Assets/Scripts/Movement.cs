a37f03d3c99d96e292533d617d8f5846d4dd0322
mrecksie <71691652+mrecksie@users.noreply.github.com>
2020-10-02 19:19:06 -0600

    Added Bombman & movement

---
+++ Bomb Bomb Man/Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed, timer;
+    public Vector2 movement;
+
+    Rigidbody2D rb;
+    float ms;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Explode();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        ms = moveSpeed * Time.fixedDeltaTime;
+
+        rb.MovePosition(rb.position + movement * ms);
+    }
+
+    void Explode()
+    {
+        Debug.Log("I have gone boom");
+    }
+}
+
