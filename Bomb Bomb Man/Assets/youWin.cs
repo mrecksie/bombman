911bd47307ed4e289895e5d08f14be67b11da096
BigBoiBif <39178460+BigBoiBif@users.noreply.github.com>
2020-10-05 15:42:44 -0600

    End Screen
    added end screen, changed backgrounds from blue to black

---
+++ Bomb Bomb Man/Assets/youWin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class youWin : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
